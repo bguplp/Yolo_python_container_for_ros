d090ca021fe5e4a50dea407fd490cfeeb3f36c1a
lar0 <alexmo@post.bgu.ac.il>
2019-09-26 11:48:55 +0000

    Removed auto-compiled files

--- build/catkin_generated/order_packages.py
+++
@@ -1,5 +0,0 @@
-# generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = "/home/lar0/alex_project_ros_ws/src"
-whitelisted_packages = "".split(';') if "" != "" else []
-blacklisted_packages = "".split(';') if "" != "" else []
-underlay_workspaces = "/home/lar0/ivo_alex/ivo/devel;/opt/ros/kinetic".split(';') if "/home/lar0/ivo_alex/ivo/devel;/opt/ros/kinetic" != "" else []
